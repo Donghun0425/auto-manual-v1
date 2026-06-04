fa2ce5fe0cc9c738cb5347d02f14dfc69a1b594c
Donghun0425 <yeom6460@naver.com>
2026-06-04 10:24:08 +0900

    feat(env): Supabase Key, Proxcy 설정, IP대역 추가

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  allowedDevOrigins: ["172.16.100.30", "192.168.71.*"],
+  allowedDevOrigins: ["172.16.100.*", "192.168.71.*"],
 };
 
 export default nextConfig;
